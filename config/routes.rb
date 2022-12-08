682f3bafd09c9d4f0a4f0e885df8839d7f857b8b
Gavin Nodwell <gnodwell@cis3260.socs.uoguelph.ca>
2022-12-07 21:19:15 -0500

    Front end progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "game#index"
+
+  get "/queue_page", to: 'game#queue_page'
+  get "/game_page", to: 'game#game_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount ActionCable.server => "/cable"
 end
