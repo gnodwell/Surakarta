9fc05ee2f2f7b67b48b2375dd973a645ffed5ca3
TyETets <tykema@uoguelph.ca>
2022-12-09 14:12:38 -0500

    coin class implemented and added

--- app/helpers/coin.rb
+++ app/helpers/coin.rb
@@ -1,3 +1,23 @@
-Class Coin {
-    
-}
+class Coin
+    @face
+
+    def __init__(face)
+        @face = face
+    end
+
+    def get_face()
+        return @face
+    end
+
+    def flip()
+        side = rand(1..2)
+
+        if side == 1
+            @face = :H
+        else
+            @face = :T
+        end
+
+        return self
+    end
+end
