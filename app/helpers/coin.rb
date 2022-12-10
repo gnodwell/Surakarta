723d206fbdd9639529ecc490eba62243219920af
TyETets <tykema@uoguelph.ca>
2022-12-09 23:53:54 -0500

    updated coin to be more intuitive

--- app/helpers/coin.rb
+++ app/helpers/coin.rb
@@ -1,23 +1,17 @@
 class Coin
+  # Initialize the class with a random "face" (either "heads" or "tails")
+  def initialize
+    @face = [:H, :T].sample
+  end
+
+  # Flip the coin by randomly choosing a new face
+  def flip
+    @face = [:H, :T].sample
+  end
+
+  # Return the current face of the coin
+  def face
     @face
-
-    def __init__(face)
-        @face = face
-    end
-
-    def get_face()
-        return @face
-    end
-
-    def flip()
-        side = rand(1..2)
-
-        if side == 1
-            @face = :H
-        else
-            @face = :T
-        end
-
-        return self
-    end
+  end
 end
+  
