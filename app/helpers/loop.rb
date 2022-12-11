e3b79e20657e405e5665cd3308f6cda2c26dce0a
Parth Miglani <pmiglani@uoguelph.ca>
2022-12-11 18:44:34 -0500

    update

--- app/helpers/loop.rb
+++ app/helpers/loop.rb
@@ -8,6 +8,6 @@ class Loop {
     end
 
     def getLoopEnd(cell)
-        # Uses the PathMap model to get the other end of the loop given a cell, returns false if cell is not an entry to a loop
+      # Uses the PathMap model to get the other end of the loop given a cell, returns false if cell is not an entry to a loop
     end
 }
