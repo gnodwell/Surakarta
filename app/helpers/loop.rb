efa7a6a6c7867426ddeee370adf9da50b15414b9
Parth Miglani <pmiglani@uoguelph.ca>
2022-12-10 20:20:18 -0500

    update loop class

--- app/helpers/loop.rb
+++ app/helpers/loop.rb
@@ -1,4 +1,13 @@
 class Loop {
-    colour: nil
-    cells: nil
+    # Enum for piece colors
+    Color = Enum.new(:black, :white)
+  
+    def initialize(color, cell)
+      @color = Color[color]
+      @cell = cell
+    end
+
+    def getLoopEnd(cell)
+        # Uses the PathMap model to get the other end of the loop given a cell, returns false if cell is not an entry to a loop
+    end
 }
