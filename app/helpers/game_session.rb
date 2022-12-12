1f73da2abbae1a8bd5aa16008863f0778d1708b9
TyETets <tykema@uoguelph.ca>
2022-12-11 19:09:09 -0500

    Adjusted functionality to match design document

--- app/helpers/game_session.rb
+++ app/helpers/game_session.rb
@@ -17,16 +17,16 @@ class GameSession < ApplicationRecord
   def getResults()
     # no given way to implement this in given documentation, so...
 
-    # returns winner and loser in player array
-    player_result = []
-    for player in @SessionHost.get_players # will need SessionHost to store list of players, and have a method to retrieve them
-      if player.piece_count == 0 # player will need a method to return the number of pieces they have
-        player_result.push(player)
-      elsif player.piece_count > 0 && player_result.length() > 0
-        player_result.push(player)
-      end
-    end
-    return player_result
+    ### This is the only way I could think of to get the winner and loser using the given documentation, so...
+    # player_result = []
+    # for player in @SessionHost.get_players # will need SessionHost to store list of players, and have a method to retrieve them
+    #   if player.piece_count == 0 # player will need a method to return the number of pieces they have
+    #     player_result.push(player)
+    #   elsif player.piece_count > 0 && player_result.length() > 0
+    #     player_result.push(player)
+    #   end
+    # end
+    # return player_result
   end
 
   def restartGame()
