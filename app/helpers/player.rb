7c89005773fc35428b3c29af3885c4fbdddbd062
Parth Patel <ppatel23@uoguelph.ca>
2022-12-11 19:33:34 -0500

    Added commented potential implementations

--- app/helpers/player.rb
+++ app/helpers/player.rb
@@ -8,23 +8,25 @@ class Player
     @boardState = nil
     @host = nil
 
+    #@restart --> Assuming this is another instance variable that needs to be created
 
     def initialize() 
-       #im assuming name is passed in the initialization
+       #Assuming name is passed in the initialization
        #@name = name
     end
 
     # --------------------- PUBLIC METHODS ---------------------
 
     def checkRestart()
-        #is there a restart var that is to be checked?
+        #is there a restart var that is to be checked? If so it can implemented as such:
         #return @restart 
     end
 
-    def setRestart(restartValue)
+    def setRestart()
+        #Assuming a restartValue is passed as an argument (e.g. setRestart(restartValue))
+        #Is there a restart var that is to be checked? If so it can implemented as such:
         #@restart = restartValue
     end
-
     # --------------------- PRIVATE METHODS ---------------------
     private
 end
